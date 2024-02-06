de321d04b9968082c9e2c700d359fde8a351e380
nayan <nayan@appsmith.com>
2024-02-06 15:59:28 +0600

    Added jsobject

---
+++ pages/Page2/jsobjects/MyJsObj/MyJsObj.js
@@ -0,0 +1,11 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	sayHelloWorld () {
+		return "Hello World";
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
